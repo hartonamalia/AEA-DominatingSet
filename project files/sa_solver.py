810962c8ce3d285cdc87d3cbc278316af6b8ee9b
andrei-ioan-epure <epureandrei567@gmail.com>
2025-04-28 21:21:39 +0300

    Multiple Neighbor Generation

--- project files/sa_solver.py
+++ project files/sa_solver.py
@@ -39,45 +39,75 @@ class DominatingSetSA:
 
         return solution
 
-    def get_neighbor(self, current_solution):
+    def get_neighbors(self, current_solution, num_neighbors=5):
+
         all_nodes = list(self.graph.nodes())
-        non_solution_nodes = [n for n in all_nodes if n not in current_solution]
+        neighbors = []
 
-        strategy = random.random()
+        for _ in range(num_neighbors):
+            non_solution_nodes = [n for n in all_nodes if n not in current_solution]
+            strategy = random.random()
 
-        if len(current_solution) == 0 or (len(current_solution) < len(all_nodes) / 2 and strategy < 0.3):
-            if non_solution_nodes:
+            if len(current_solution) == 0 or (len(current_solution) < len(all_nodes) / 2 and strategy < 0.3):
+                if non_solution_nodes:
+                    new_solution = current_solution.copy()
+                    new_solution.append(random.choice(non_solution_nodes))
+                    neighbors.append(new_solution)
+                    continue
+
+            elif len(current_solution) > 1 and strategy < 0.5:
+                redundant_nodes = []
+                for node in current_solution:
+                    temp_solution = [n for n in current_solution if n != node]
+                    if self.is_dominating_set(temp_solution):
+                        redundant_nodes.append(node)
+
                 new_solution = current_solution.copy()
-                new_solution.append(random.choice(non_solution_nodes))
-                return new_solution
+                if redundant_nodes:
+                    new_solution.remove(random.choice(redundant_nodes))
+                elif len(current_solution) > 1:
+                    new_solution.remove(random.choice(current_solution))
 
-        elif len(current_solution) > 1 and strategy < 0.5:
-            redundant_nodes = []
-            for node in current_solution:
-                temp_solution = [n for n in current_solution if n != node]
-                if self.is_dominating_set(temp_solution):
-                    redundant_nodes.append(node)
+                neighbors.append(new_solution)
+                continue
 
-            if redundant_nodes:
+            elif non_solution_nodes and current_solution:
                 new_solution = current_solution.copy()
-                new_solution.remove(random.choice(redundant_nodes))
-                return new_solution
-            else:
-                new_solution = current_solution.copy()
-                new_solution.remove(random.choice(current_solution))
-                return new_solution
+                node_to_remove = random.choice(current_solution)
+                node_to_add = random.choice(non_solution_nodes)
+                new_solution.remove(node_to_remove)
+                new_solution.append(node_to_add)
+                neighbors.append(new_solution)
+                continue
 
-        elif non_solution_nodes and current_solution:
-            new_solution = current_solution.copy()
-            node_to_remove = random.choice(current_solution)
-            node_to_add = random.choice(non_solution_nodes)
-            new_solution.remove(node_to_remove)
-            new_solution.append(node_to_add)
-            return new_solution
+            neighbors.append(current_solution.copy())
 
-        return current_solution.copy()
 
-    def solve(self, initial_temp=100.0, final_temp=0.1, cooling_rate=0.95, iterations_per_temp=100, time_limit=300):
+        unique_neighbors = []
+        seen = set()
+        for neighbor in neighbors:
+            neighbor_tuple = tuple(sorted(neighbor))
+            if neighbor_tuple not in seen:
+                seen.add(neighbor_tuple)
+                unique_neighbors.append(neighbor)
+
+        return unique_neighbors
+
+    def select_best_neighbor(self, neighbors):
+
+        best_neighbor = None
+        best_objective = float('inf')
+
+        for neighbor in neighbors:
+            objective = self.objective_function(neighbor)
+            if objective < best_objective:
+                best_neighbor = neighbor
+                best_objective = objective
+
+        return best_neighbor, best_objective
+
+    def solve(self, initial_temp=100.0, final_temp=0.1, cooling_rate=0.95, iterations_per_temp=100, time_limit=300,
+              num_neighbors=5):
         start_time = time.time()
         current_solution = self.generate_initial_solution()
         current_objective = self.objective_function(current_solution)
@@ -103,25 +133,28 @@ class DominatingSetSA:
                 if time.time() - start_time >= time_limit:
                     break
 
-                neighbor_solution = self.get_neighbor(current_solution)
-                neighbor_objective = self.objective_function(neighbor_solution)
+                neighbor_solutions = self.get_neighbors(current_solution, num_neighbors)
 
-                is_valid_dominating_set = self.is_dominating_set(neighbor_solution)
+                for neighbor_solution in neighbor_solutions:
+                    neighbor_objective = self.objective_function(neighbor_solution)
+                    is_valid_dominating_set = self.is_dominating_set(neighbor_solution)
 
-                delta = neighbor_objective - current_objective
+                    delta = neighbor_objective - current_objective
 
-                if delta < 0 or random.random() < math.exp(-delta / temperature):
-                    current_solution = neighbor_solution
-                    current_objective = neighbor_objective
+                    if delta < 0 or random.random() < math.exp(-delta / temperature):
+                        current_solution = neighbor_solution
+                        current_objective = neighbor_objective
 
-                    if is_valid_dominating_set and (
-                            self.best_solution is None or
-                            len(neighbor_solution) < len(self.best_solution)
-                    ):
-                        self.best_solution = neighbor_solution.copy()
-                        self.best_objective_value = len(neighbor_solution)
-                        self.optimal_solution_found = True
-                        improvement_in_this_temp = True
+                        if is_valid_dominating_set and (
+                                self.best_solution is None or
+                                len(neighbor_solution) < len(self.best_solution)
+                        ):
+                            self.best_solution = neighbor_solution.copy()
+                            self.best_objective_value = len(neighbor_solution)
+                            self.optimal_solution_found = True
+                            improvement_in_this_temp = True
+
+                        break
 
                 iteration += 1
 
